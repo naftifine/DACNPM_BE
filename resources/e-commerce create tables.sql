da32036543f1fe139dbde38e942d3ff50786d552
naftifine <hung15092004@gmail.com>
2024-12-04 06:06:02 +0700

    (BE) re-upload

--- resources/e-commerce create tables.sql
+++ resources/e-commerce create tables.sql
@@ -7,7 +7,8 @@
     Fullname NVARCHAR(100) NOT NULL,
     PhoneNumber VARCHAR(16),
     CONSTRAINT CK_CCCD CHECK (LEN(CCCD) = 12), 
-    CONSTRAINT CK_PhoneNumber CHECK (LEN(PhoneNumber) >= 10) 
+    CONSTRAINT CK_PhoneNumber CHECK (LEN(PhoneNumber) >= 10),
+    refresh_token VARCHAR(255) NULL
 );
 CREATE TABLE [store] (
     StoreID INT IDENTITY(1,1) PRIMARY KEY,
@@ -20,3 +21,81 @@ CREATE TABLE [store] (
     ON DELETE SET NULL
     ON UPDATE CASCADE
 );
+
+CREATE TABLE Category (
+    CategoryID INT IDENTITY(1,1) PRIMARY KEY,
+    CName NVARCHAR(100) UNIQUE NOT NULL,
+    Cate_Descr NVARCHAR(MAX),
+    Image NVARCHAR(MAX) -- sql k có json
+);
+
+CREATE TABLE Product (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    PName NVARCHAR(100) NOT NULL,
+    SellPrice INT NOT NULL,
+    Pr_Descr NVARCHAR(MAX),
+    Amount INT DEFAULT 0,
+    TimeUpload DATETIME NOT NULL,
+    ImagePd NVARCHAR(MAX), 
+    Attribute NVARCHAR(MAX), 
+    StatusPro NVARCHAR(10) CHECK (StatusPro IN ('ForSale', 'Sold', 'Deleted')) NOT NULL,
+    SellerID INT NOT NULL,
+    PC_ID INT,
+    FOREIGN KEY (SellerID) REFERENCES [User](UserID) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (PC_ID) REFERENCES Category(CategoryID) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE Review (
+    ReviewID INT IDENTITY(1,1) PRIMARY KEY,
+    ReviewStoreID INT NOT NULL,
+    ReviewUserID INT NOT NULL,
+    Content NVARCHAR(MAX),
+    CommentDate DATETIME NOT NULL,
+    Rating INT CHECK (Rating >= 0 AND Rating <= 5),
+    FOREIGN KEY (ReviewStoreID) REFERENCES Store(StoreID), 
+    FOREIGN KEY (ReviewUserID) REFERENCES [User](UserID) -- double cascade is not allowed -> trigger
+);
+
+
+CREATE TABLE Conservation (
+    Cons_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Date_Start DATETIME NOT NULL,
+    Title NVARCHAR(255)
+);
+
+CREATE TABLE Join_Conservation (
+    Conser_ID INT NOT NULL,
+    JoinID INT NOT NULL,
+    Con_Status NVARCHAR(10) CHECK (Con_Status IN ('Seen', 'NotSeen')),
+    PRIMARY KEY (Conser_ID, JoinID),
+    FOREIGN KEY (Conser_ID) REFERENCES Conservation(Cons_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (JoinID) REFERENCES [User](UserID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Message (
+    Mess_ID INT IDENTITY(1,1) PRIMARY KEY,
+    Date_Send DATETIME NOT NULL,
+    Content NVARCHAR(MAX) NOT NULL,
+    SenderID INT NOT NULL,
+    Mes_ConID INT NOT NULL,
+    FOREIGN KEY (SenderID) REFERENCES [User](UserID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Mes_ConID) REFERENCES Conservation(Cons_ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Article (
+    ArticleID INT IDENTITY(1,1) PRIMARY KEY,
+    ArticleStoreId INT NOT NULL,
+    Title NVARCHAR(255) NOT NULL,
+    Content NVARCHAR(MAX),
+    ImageAr NVARCHAR(MAX),
+    created_at DATETIME,
+    LikeAr INT DEFAULT 0,
+    FOREIGN KEY (ArticleStoreId) REFERENCES Store(StoreID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Admin (
+    Username NVARCHAR(255) PRIMARY KEY,
+    Passw NVARCHAR(100) NOT NULL
+);
+
+
